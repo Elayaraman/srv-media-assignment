d089ad7598fe4d2b57605593ffa5305bc253d0cf
Elayaraman <elayaraman007@gmail.com>
2026-06-28 10:00:22 +0530

    style: adjust brand logo dimensions in navigation header to match exact 205x160px figma specifications

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,12 +19,12 @@ export const Header = () => {
     <header className="sticky top-0 z-50 w-full h-[100px] bg-white border-b border-gray-100 shadow-sm">
       <div className="max-w-[1920px] mx-auto h-full flex items-center justify-between px-6 md:px-12 lg:px-[76px] relative">
         {/* Left side brand logo (absolute to allow overlapping hang-down) */}
-        <div className="relative h-full w-[201px]">
+        <div className="relative h-full w-[205px]">
           <a href="/" className="block h-full">
             <img
               src="/images/pse_logo.png"
               alt="Premier Schools Exhibition"
-              className="absolute top-[3px] left-0 w-[201px] h-[157px] max-w-none z-20 transition-transform duration-300 hover:scale-[1.02] drop-shadow-md"
+              className="absolute top-0 left-0 w-[205px] h-[160px] max-w-none z-20 transition-transform duration-300 hover:scale-[1.02] drop-shadow-md"
             />
           </a>
         </div>
